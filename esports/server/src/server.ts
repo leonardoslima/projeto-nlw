cf57c4f6fe5e7dd534ac44269aa995dbaf7ee792
Leonardo <leonardosilvalima93@live.com>
2022-09-18 23:47:27 -0300

    Aula 5 Projeto Finalizado

--- esports/server/src/server.ts
+++ esports/server/src/server.ts
@@ -1,7 +1,7 @@
-import express, { query, request, response } from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 import { convertHourStringToMinutes } from "./utils/convert-hour-string-to-minutes";
 import { convertMinutesToHourString } from "./utils/convert-minutes-to-hour-string";
 
@@ -11,24 +11,24 @@ app.use(express.json());
 app.use(cors());
 
 const prisma = new PrismaClient({
-  log: ["query"],
+  log: ['query'],
 });
 
-app.get("/games", async (request, response) => {
+app.get('/games', async (request, response) => {
   const games = await prisma.game.findMany({
     include: {
       _count: {
         select: {
           ads: true,
-        },
-      },
-    },
+        }
+      }
+    }
   });
 
-  return response.json([games]);
+  return response.json(games);
 });
 
-app.post("/games/:id/ads", async (request, response) => {
+app.post('/games/:id/ads', async (request, response) => {
   const gameId = request.params.id;
   const body: any = request.body;
 
@@ -38,24 +38,24 @@ app.post("/games/:id/ads", async (request, response) => {
       name: body.name,
       yearsPlaying: body.yearsPlaying,
       discord: body.discord,
-      weekDay: body.weekDay.join(","),
+      weekDays: body.weekDays.join(','),
       hourStart: convertHourStringToMinutes(body.hourStart),
       hourEnd: convertHourStringToMinutes(body.hourEnd),
       useVoiceChannel: body.useVoiceChannel,
     },
-  });
+  })
 
-  return response.status(201).json(body);
+  return response.status(201).json(ad);
 });
 
-app.get("/games/:id/ads", async (request, response) => {
+app.get('/games/:id/ads', async (request, response) => {
   const gameId = request.params.id;
 
   const ads = await prisma.ad.findMany({
     select: {
       id: true,
       name: true,
-      weekDay: true,
+      weekDays: true,
       useVoiceChannel: true,
       yearsPlaying: true,
       hourStart: true,
@@ -65,23 +65,21 @@ app.get("/games/:id/ads", async (request, response) => {
       gameId,
     },
     orderBy: {
-      createdAt: "desc",
-    },
-  });
+      createdAt: 'desc'
+    }
+  })
 
-  return response.json(
-    ads.map((ad) => {
-      return {
-        ...ad,
-        weekDay: ad.weekDay.split(","),
-        hourStart: convertMinutesToHourString(ad.hourStart),
-        hourEnd: convertMinutesToHourString(ad.hourEnd),
-      };
-    })
-  );
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(','),
+      hourStart: convertMinutesToHourString(ad.hourStart),
+      hourEnd: convertMinutesToHourString(ad.hourEnd),
+    }
+  }));
 });
 
-app.get("/ads/:id/discord", async (request, response) => {
+app.get('/ads/:id/discord', async (request, response) => {
   const adId = request.params.id;
 
   const ad = await prisma.ad.findUniqueOrThrow({
@@ -90,12 +88,12 @@ app.get("/ads/:id/discord", async (request, response) => {
     },
     where: {
       id: adId,
-    },
+    }
   });
 
   return response.json({
     discord: ad.discord,
-  });
+  })
 });
 
 app.listen(3333);
--- esports/server/src/utils/convert-hour-string-to-minutes.ts
+++ esports/server/src/utils/convert-hour-string-to-minutes.ts
@@ -1,7 +1,7 @@
 export function convertHourStringToMinutes(hourString: string) {
-  const [hours, minutes] = hourString.split(":").map(Number);
+  const [hour, minutes] = hourString.split(':').map(Number);
 
-  const minutesAmount = hours * 60 + minutes;
+  const minutesAmount = hour * 60 + minutes;
 
   return minutesAmount;
 }
